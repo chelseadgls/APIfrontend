83089cb32cdf0b6a74c35e5a573ca877a15aaf5c
chelseadgls <chelseamdouglas@gmail.com>
2022-10-31 15:19:40 -0400

    initial commit

---
+++ apifrontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import APIData from './components/APIData';
+
+function App() {
+
+  return (
+    <div className="App">
+      <APIData />
+    </div>
+  );
+}
+
+export default App;
